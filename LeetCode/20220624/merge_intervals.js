0d83967daedc5bd79da526fcbede35cf2d6ecdbb
Kyle Finkernagel <Kyle.Finkernagel@gmail.com>
2022-06-24 14:45:45 -0500

    mergeintervals complete 23:00

--- LeetCode/20220624/merge_intervals.js
+++ LeetCode/20220624/merge_intervals.js
@@ -1,11 +1,27 @@
 /**
  * @param {number[][]} intervals
  * @return {number[][]}
-   Input: intervals = [[1,3],[2,6],[8,10],[15,18]]
-   Output: [[1,6],[8,10],[15,18]]
-   Explanation: Since intervals [1,3] and [2,6] overlap, merge them into [1,6].
- * 
  */
  var merge = function(intervals) {
-    
+    intervals.sort((a,b) => a[0] - b[0])
+    for(i=0; i<intervals.length-1; ++i) {
+        if(intervals[i][1]>=intervals[i+1][0]) {
+            intervals[i][1] = Math.max(intervals[i+1][1], intervals[i][1])
+            intervals.splice(i+1,1)
+            --i
+        }
+    }
+    return intervals
 };
+
+module.exports = merge
+
+// Problem 1: [[1,4],[0,4]]
+//  first solution failure did not account for the first value of the second compare
+//  to be a lesser value than the first array.  Solved with a sort function.
+// Problem 2: [[1,4],[2,3]]
+//  second solution failure did not account for the second array to be contained fully
+//  in the first array.
+// Problem 3: [[1,4],[0,2],[3,5]]
+//  the program did not account for stepping back to check the new array inserted against
+//  the next value.  Added a step back action to make sure the function checked.
--- LeetCode/20220624/merge_intervals.test.js
+++ LeetCode/20220624/merge_intervals.test.js
@@ -1,5 +1,28 @@
 const merge = require('./merge_intervals')
 
+
+// Problem 1: [[1,4],[0,4]]
+//  first solution failure did not account for the first value of the second compare
+//  to be a lesser value than the first array.  Solved with a sort function.
+// Problem 2: [[1,4],[2,3]]
+//  second solution failure did not account for the second array to be contained fully
+//  in the first array.
+// Problem 3: [[1,4],[0,2],[3,5]]
+//  the program did not account for stepping back to check the new array inserted against
+//  the next value.  Added a step back action to make sure the function checked.
+
 test("An array with overlapping members", () => {
-    expect(merge([[1,3],[2,6],[8,10],[15,18]]).toStrictEqual([[1,6],[8,10],[15,18]]))
+    expect(merge([[1,3],[2,6],[8,10],[15,18]])).toStrictEqual([[1,6],[8,10],[15,18]])
+})
+
+test("An array with no overlapping members", () => {
+    expect(merge([[0,1],[2,3]])).toStrictEqual([[0,1],[2,3]])
+})
+
+test("An array where the second entry contains the first", () => {
+    expect(merge([[1,4],[0,4]])).toStrictEqual([[0,4]])
+})
+
+test("An array where the second entry is fully contained in the first", () => {
+    expect(merge([[1,4],[2,3]])).toStrictEqual([[1,4]])
 })
