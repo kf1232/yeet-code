a020aef967b07ab82cb7e144034b7f2c09ff8dfa
Kyle Finkernagel <Kyle.Finkernagel@gmail.com>
2022-07-04 16:58:46 -0500

    roman cleanup

--- LeetCode/20220704/romanToInt.js
+++ LeetCode/20220704/romanToInt.js
@@ -5,14 +5,15 @@
 var romanToInt = function(s) {
     if(s.length == 0) return 0
 
-    let c = {}
-    c['I'] = 1
-    c['V'] = 5
-    c['X'] = 10
-    c['L'] = 50
-    c['C'] = 100
-    c['D'] = 500
-    c['M'] = 1000
+    const c = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000,
+    }
     
     let output = c[s[s.length-1]]
     for( let i = s.length-2; i >= 0; --i) {
